58e310cedb75dc3259d86e6db049ece215f51d2a
gnikolaos <dizars1776@yahoo.gr>
2025-03-08 00:00:27 +0200

    fix(Home): temporary fix until I bring back the menu or whatever

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,4 @@
 import Footer from '@gnikolaos/components/Footer'
-import Header from '@gnikolaos/components/Header'
 import BackgroundEffect from '@gnikolaos/components/BackgroundEffect'
 import { IconEmail, IconGitHub, IconLinkedIn } from '@gnikolaos/components/Icon'
 
@@ -7,8 +6,7 @@ const Home = () => {
     return (
         <>
             <BackgroundEffect />
-            <Header />
-            <main class="z-10 flex-1">
+            <main class="z-10 flex-1 pt-[86px]">
                 <div class="container mx-auto flex h-full flex-col items-center justify-center gap-4 text-center">
                     <h1 class="-mt-24 text-6xl font-bold text-white drop-shadow-md md:text-8xl">Georgiou Nikolaos</h1>
                     <h2 class="text-xl font-bold text-white drop-shadow-md md:text-2xl">
@@ -17,18 +15,21 @@ const Home = () => {
                     <div class="flex gap-6 pt-8">
                         <a
                             href="https://github.com/gnikolaos"
+                            aria-label="Check out my GitHub profile"
                             class="rounded-sm p-2 text-stone-400 transition-colors hover:text-white focus-visible:outline-stone-400"
                         >
                             <IconGitHub twClass="h-7 w-7" />
                         </a>
                         <a
                             href="mailto:nikos127.0.0.1@hotmail.com"
+                            aria-label="Send me a business inquiry"
                             class="rounded-sm p-2 text-stone-400 transition-colors hover:text-white focus-visible:outline-stone-400"
                         >
                             <IconEmail twClass="h-7 w-7" />
                         </a>
                         <a
                             href="/"
+                            aria-label="Connect with me on LinkedIn"
                             class="rounded-sm p-2 text-stone-400 transition-colors hover:text-white focus-visible:outline-stone-400"
                         >
                             <IconLinkedIn twClass="h-7 w-7" />
