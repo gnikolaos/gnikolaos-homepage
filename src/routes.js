843fe4003d00ed64cbe6f51aa94e467207bc8b50
gnikolaos <dizars1776@yahoo.gr>
2025-03-12 22:44:54 +0200

    feat: terminal's route base

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,16 @@ const routes = [
         path: '/',
         component: lazy(() => import('./routes/Home')),
     },
+    {
+        path: '/terminal',
+        component: lazy(() => import('./routes/Terminal')),
+        children: [
+            {
+                path: '/:path?/:fileName?',
+                component: lazy(() => import('./routes/Terminal')),
+            },
+        ],
+    },
 ]
 
 export default routes
---
+++ src/routes/Terminal.jsx
@@ -0,0 +1,13 @@
+/**
+ * Terminal route simulates a Unix-like "command line" interface.
+ * Allows users to input commands and view outputs.
+ */
+const Terminal = () => {
+    return (
+        <div>
+            <h1 class="text-xl">Terminal's page</h1>
+        </div>
+    )
+}
+
+export default Terminal
