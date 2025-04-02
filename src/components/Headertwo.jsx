f9994a56ea5e8c5c3aac10b7dd0ac463b4c3f542
gnikolaos <dizars1776@yahoo.gr>
2025-04-02 23:06:50 +0300

    fix(Headertwo): rj45 cable

--- src/components/Headertwo.jsx
+++ src/components/Headertwo.jsx
@@ -4,9 +4,26 @@ const Headertwo = () => {
             <div class="relative ml-4 rounded-sm bg-gray-300/10 px-5 py-2">
                 {/* RJ45 socket */}
                 <div class="flex size-4 items-start justify-end gap-[1px] rounded-xs bg-gray-300 pt-0.5 pr-0.5 shadow-md">
-                    <span class="size-0.5 rounded-full bg-red-500"></span>
-                    <span class="size-0.5 rounded-full bg-gray-400"></span>
-                    <span class="size-0.5 rounded-full bg-gray-400"></span>
+                    <span class="size-0.5 rounded-full bg-red-500" />
+                    <span class="size-0.5 rounded-full bg-gray-400" />
+                    <span class="size-0.5 rounded-full bg-gray-400" />
+                </div>
+            </div>
+            <div class="relative w-dvw py-2">
+                {/* RJ45 cable */}
+                <div class="flex items-center">
+                    <div class="flex h-2 w-1.5 items-center justify-center border border-gray-300">
+                        <div class="h-1 w-[1px] bg-gray-300" />
+                    </div>
+                    <div class="h-2.5 w-2 bg-gray-300 pr-0.5">
+                        <div class="flex size-full items-center justify-end">
+                            <div class="flex h-10/12 w-1 items-center justify-end border border-gray-700">
+                                <span class="size-[1px] bg-gray-700" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="h-3 w-2 bg-gray-300" />
+                    <div class="ml-[1px] h-1 w-full bg-gray-300" />
                 </div>
             </div>
         </header>
