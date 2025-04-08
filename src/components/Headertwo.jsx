5c685419cf96607c50144935e9b6de1fd8dff1f5
gnikolaos <dizars1776@yahoo.gr>
2025-04-08 18:25:03 +0300

    fix(Headertwo): rj45 socket sizing

--- src/components/Headertwo.jsx
+++ src/components/Headertwo.jsx
@@ -3,10 +3,10 @@ const Headertwo = () => {
         <header class="container mx-auto flex justify-between py-8 text-xs md:px-8 md:text-base">
             <div class="relative ml-4 rounded-sm bg-gray-300/10 px-5 py-2">
                 {/* RJ45 socket */}
-                <div class="flex size-4 items-start justify-end gap-[1px] rounded-xs bg-gray-300 pt-0.5 pr-0.5 shadow-md">
-                    <span class="size-0.5 rounded-full bg-red-500" />
-                    <span class="size-0.5 rounded-full bg-gray-400" />
-                    <span class="size-0.5 rounded-full bg-gray-400" />
+                <div class="flex size-11 items-start justify-end gap-0.5 rounded-xs bg-gray-300 pt-0.5 pr-0.5 shadow-md">
+                    <span class="size-1.5 rounded-full bg-red-500" />
+                    <span class="size-1.5 rounded-full bg-gray-400" />
+                    <span class="size-1.5 rounded-full bg-gray-400" />
                 </div>
             </div>
             <div class="relative w-dvw py-2">
